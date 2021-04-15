a0e7e3912c4d5915d4c12645ef486e662995ac39
Dayunningtias <diah.19411044@student.ubl.ac.id>
2021-04-15 11:45:31 +0700

    latihan1

---
+++ index.js
@@ -0,0 +1,22 @@
+// const ListNilai = ['A', 'B', 'C', 'D']
+
+// console.log(ListNilai[1])
+
+const dataMhs = [
+    {
+        nama : 'Diah Ayu Ningtias',
+        npm : '19411044'
+    },
+    {
+        nama : 'Any Hardiany',
+        npm : 19411075
+    },
+    {
+        nama : 'Kanara Cita',
+        npm : 19411035
+    }
+    ]
+    
+    for (let i = 0; i < dataMhs.length; i++) {
+        console.log (dataMhs [i].npm)
+    } 
